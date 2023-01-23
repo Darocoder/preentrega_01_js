0401dc5a25fe4ebab6af826a6c7755f7f9e389b5
Daro <Daro@iMac-de-Malaga.local>
2023-01-22 22:22:01 -0300

    Agrego arrays y métodos de búsqueda

--- Preentrega_01/js/script.js
+++ Preentrega_01/js/script.js
@@ -1,11 +1,7 @@
-// let total = 0
-// let cantidadProductos = 0
-// let numeroLista = 0
-// const salir = "esc"
-
+//Funciones a utilizar en el programa
 function ingresaProducto() {
     let p = prompt ("Ingrese un producto o escriba ESC para salir");
-    
+    listaProducto.push (p);
     while (p == "") {
         p = prompt ("El producto ingresado está en blanco, ingrese un producto o escriba ESC para salir")
     }
@@ -14,38 +10,49 @@ function ingresaProducto() {
 
 function ingresaCantidad() {
     let c = prompt ("Ingrese la cantida deseada");
+    listaCantidad.push (c)
     while (c =="") {
         c = prompt ("La cantidad ingresada está en blanco, ingrese una cantidad")
     }
     return c
 }
 
+//Constantes
+const listaProducto = []
+const listaCantidad = []
 const cantidadMaxima = 3
-alert ("Bienvenido a tu lista de compra, puedes ingresar hasta " + cantidadMaxima + " items a comprar")
 
+//Inicio del programa
+alert ("Bienvenido a tu lista de compra, puedes ingresar hasta " + cantidadMaxima + " items a comprar");
 
+//Solicito productos y cantidades dentro de un ciclo FOR //Establezco ESC como variable de salida
 // console.log ("El contenido de i es: " + i)
 let cantidadTotalDeProductos = 0;
-for (let i = 1; i <= cantidadMaxima; i++) {    
-    let producto = ingresaProducto();
+let producto;
+let cantidad;
+let i;
+for (i = 1; i <= cantidadMaxima; i++) {    
+    producto = ingresaProducto();
     if (producto == "ESC") {
         console.log ("Has salido de la lista de compra con " + cantidadTotalDeProductos + " artículos")
         break
     }
-    let cantidad = ingresaCantidad ()
+    cantidad = ingresaCantidad ()
     
     cantidadTotalDeProductos = Number (cantidadTotalDeProductos) + Number (cantidad)  //cantidadTodalDeProductos += cantidad
     console.log ("Item nº " + i + " ---> " + cantidad + " unidades de " + producto)  
     console.log  ("Total de productos: " + cantidadTotalDeProductos)
 }
-// console.log ("El contenido de i es: " + i)
 
+//Muestro lista creada al final
+let textoAmostrar = "Lista de compra creada:\n"
+for (i = 0; i<listaCantidad.length; i++){
+    textoAmostrar+="Artículo: " + listaProducto[i] + "\t" + "Cantidad: " + listaCantidad[i] + "\n";
+}
+alert (textoAmostrar);
 
+//Pregunto (búsqueda) por un artículo en particular
+productoBuscado = prompt ("Ingrese un producto para saber la cantidad a comprar")
+posiciónProductoBuscado = listaProducto.indexOf(productoBuscado)
+alert ("La cantidad a comprar de " + productoBuscado + " es " + listaCantidad[posiciónProductoBuscado] + " unidades" )
 
-
-
-
-
-// if (total == 10) { 
-//     alert ("Llegaste a 10 productos en tu lista") 
-// }
