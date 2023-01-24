412aad4c34bd13118df5602320cd8bbc9b8534a0
Daro <Daro@iMac-de-Malaga.local>
2023-01-24 00:15:41 -0300

    Agrego nuevas funcionalidades sin objetos

--- Preentrega_01/js/script.js
+++ Preentrega_01/js/script.js
@@ -3,56 +3,76 @@ function ingresaProducto() {
     let p = prompt ("Ingrese un producto o escriba ESC para salir");
     listaProducto.push (p);
     while (p == "") {
-        p = prompt ("El producto ingresado está en blanco, ingrese un producto o escriba ESC para salir")
+        p = prompt ("El producto ingresado está en blanco, ingrese un producto o escriba ESC para salir");
     }
-    return p
+    return p;
 }
 
 function ingresaCantidad() {
     let c = prompt ("Ingrese la cantida deseada");
-    listaCantidad.push (c)
+    listaCantidad.push (c);
     while (c =="") {
-        c = prompt ("La cantidad ingresada está en blanco, ingrese una cantidad")
+        c = prompt ("La cantidad ingresada está en blanco, ingrese una cantidad");
     }
-    return c
+    return c;
 }
 
-//Constantes
-const listaProducto = []
-const listaCantidad = []
-const cantidadMaxima = 3
+//Variables
+let listaProducto = [];
+let listaCantidad = [];
 
 //Inicio del programa
-alert ("Bienvenido a tu lista de compra, puedes ingresar hasta " + cantidadMaxima + " items a comprar");
+alert ("Bienvenido a tu lista de compra, puedes ingresar los items a comprar");
 
 //Solicito productos y cantidades dentro de un ciclo FOR //Establezco ESC como variable de salida
 // console.log ("El contenido de i es: " + i)
 let cantidadTotalDeProductos = 0;
 let producto;
 let cantidad;
-let i;
-for (i = 1; i <= cantidadMaxima; i++) {    
+let i = 0;
+//for (i = 1; i <= cantidadMaxima; i++) {   
+producto = ingresaProducto();
+while (producto != "ESC") {
+    i++;
+    //if (producto == "ESC") {
+      //  console.log ("Has salido de la lista de compra con " + cantidadTotalDeProductos + " artículos")
+        //break
+    //}
+    cantidad = ingresaCantidad();
+    //cantidadTotalDeProductos = Number (cantidadTotalDeProductos) + Number (cantidad)  
+    cantidadTotalDeProductos += Number(cantidad);
+    console.log ("Item nº " + i + " ---> " + cantidad + " unidades de " + producto);  
+    console.log  ("Total de productos: " + cantidadTotalDeProductos);
     producto = ingresaProducto();
-    if (producto == "ESC") {
-        console.log ("Has salido de la lista de compra con " + cantidadTotalDeProductos + " artículos")
-        break
-    }
-    cantidad = ingresaCantidad ()
-    
-    cantidadTotalDeProductos = Number (cantidadTotalDeProductos) + Number (cantidad)  //cantidadTodalDeProductos += cantidad
-    console.log ("Item nº " + i + " ---> " + cantidad + " unidades de " + producto)  
-    console.log  ("Total de productos: " + cantidadTotalDeProductos)
 }
 
 //Muestro lista creada al final
-let textoAmostrar = "Lista de compra creada:\n"
-for (i = 0; i<listaCantidad.length; i++){
-    textoAmostrar+="Artículo: " + listaProducto[i] + "\t" + "Cantidad: " + listaCantidad[i] + "\n";
+let textoAmostrar = "Lista de compra creada:\n";
+for (i = 0; i < listaCantidad.length; i++){
+    textoAmostrar += "Artículo: " + listaProducto[i] + "\t" + "Cantidad: " + listaCantidad[i] + "\n";
 }
 alert (textoAmostrar);
 
 //Pregunto (búsqueda) por un artículo en particular
-productoBuscado = prompt ("Ingrese un producto para saber la cantidad a comprar")
-posiciónProductoBuscado = listaProducto.indexOf(productoBuscado)
-alert ("La cantidad a comprar de " + productoBuscado + " es " + listaCantidad[posiciónProductoBuscado] + " unidades" )
+let cantidadEncontrada;
+let productoBuscado = prompt ("Ingrese un producto para saber la cantidad a comprar");
+let posicionProductoBuscado = listaProducto.indexOf(productoBuscado);
+if (posicionProductoBuscado == -1){
+    cantidadEncontrada = 0;
+}
+else{
+    cantidadEncontrada = listaCantidad[posicionProductoBuscado];
+}
 
+alert ("La cantidad a comprar de " + productoBuscado + " es " + cantidadEncontrada + " unidades" );
+
+/* const listadoCursos = [
+    {nombre: 'Javascript', precio: 15000},
+    {nombre: 'ReactJS', precio: 22000},
+]
+
+const resultado = listadoCursos.find((curso) => curso.nombre === "ReactJS")
+const resultado2 = listadoCursos.find((curso) => curso.nombre === "DW")
+
+console.log(resultado) // {nombre: 'ReactJS', precio: 22000}
+console.log(resultado2) // undefined */
