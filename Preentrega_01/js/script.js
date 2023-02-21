737ad855e369370a063d2277be449e9537faefae
Daro <Daro@iMac-de-Malaga.local>
2023-02-21 16:33:34 -0300

    Agrego la lógica para poder borrar un item por id, en vez de borrar el último

--- Preentrega_01/js/script.js
+++ Preentrega_01/js/script.js
@@ -8,43 +8,76 @@ let cantidadAcumulada = 0;
 let listaDeCompras = [];
 
 
+//  variable global (tengo que acceder desde un evento)
+let numProducto = 0;
 // recupero del storage la compra anterior (por si recarga la página)
 let listaStorage = JSON.parse(sessionStorage.getItem("listaDeCompras"));
 if (listaStorage) {
-for (let compraRecuperadaDelStorage of listaStorage ){
-    listaDeCompras.push(compraRecuperadaDelStorage);
-    let li = document.createElement("li");
-    //li.innerHTML = "Almacenado: " + "Producto: " + compraRecuperadaDelStorage.producto + "  ----->  " + compraRecuperadaDelStorage.cantidad + "unidades";
 
-    // dentro del li, va un div, que contiene otros tres divs (producto, cantidad, eliminar)
-    let divFila = document.createElement("div");
-    let divColumnaIzq = document.createElement("div");
-    let divColumnaMed = document.createElement("div");
-    let divColumnaDer = document.createElement("div");
+    for (let compraRecuperadaDelStorage of listaStorage ){
+        if (!compraRecuperadaDelStorage){
+            console.log("el producto " + numProducto + " ya fue eliminado de la lista, se recupera un null y no se agrega nada")
+            continue;
+        }
 
-    //asigno una clase css a cada div
-    divFila.className = "fila";
-    divColumnaIzq.className = "columna izquierda";
-    divColumnaMed.className = "columna centro";
-    divColumnaDer.className = "columna derecha";
+        listaDeCompras.push(compraRecuperadaDelStorage);
+        let li = document.createElement("li");
+        //li.innerHTML = "Almacenado: " + "Producto: " + compraRecuperadaDelStorage.producto + "  ----->  " + compraRecuperadaDelStorage.cantidad + "unidades";
 
-    // guardo el contenido de cada div
-    divColumnaIzq.innerHTML = compraRecuperadaDelStorage.producto;
-    divColumnaMed.innerHTML = compraRecuperadaDelStorage.cantidad;
-    divColumnaDer.innerHTML = "eliminar";
+        // dentro del li, va un div, que contiene otros tres divs (producto, cantidad, eliminar)
+        let divFila = document.createElement("div");
+        let divColumnaIzq = document.createElement("div");
+        let divColumnaMed = document.createElement("div");
+        let divColumnaDer = document.createElement("div");
 
-    // pinto de otro color los recuperados
-    divColumnaIzq.style.color = "grey";
+        //asigno una clase css a cada div
+        divFila.className = "fila";
+        li.id="fila_" + numProducto;
+        divColumnaIzq.className = "columna izquierda";
+        divColumnaMed.className = "columna centro";
+        divColumnaDer.className = "columna derecha";
 
-    // creo la estructura html con jerarquías (li contiene div, que contiene 3 divs)
-    divFila.appendChild(divColumnaIzq);
-    divFila.appendChild(divColumnaMed);
-    divFila.appendChild(divColumnaDer);
-    li.appendChild(divFila);
+        // guardo el contenido de cada div
+        divColumnaIzq.innerHTML = compraRecuperadaDelStorage.producto;
+        divColumnaMed.innerHTML = compraRecuperadaDelStorage.cantidad;
 
-    lista.appendChild(li);
-    console.log("Agregando " + compraRecuperadaDelStorage.producto);
-}
+        // pinto de otro color los recuperados
+        divColumnaIzq.style.color = "grey";
+
+        let botonBorrar = document.createElement("botton");
+        botonBorrar.innerHTML = "eliminar";
+        botonBorrar.className = "botonEliminar";
+        botonBorrar.id = numProducto;
+
+
+        // asigno una funcionalidad al botón sobre cada ítem recuperado del storage
+        botonBorrar.onclick = () => {
+            console.log("Eliminando fila " + "fila_" + botonBorrar.id);
+            console.log(listaDeCompras)
+
+            let eliminado = listaDeCompras[botonBorrar.id];
+            if (eliminado){   
+                delete listaDeCompras[botonBorrar.id];     
+                listaDeCompras.remove    
+                console.log("por borrar el elemento " + String(botonBorrar.id));
+                document.getElementById("fila_" + botonBorrar.id).remove();
+                console.log ("Has eliminado "+ eliminado.producto);
+                console.log (listaDeCompras);
+                sessionStorage.setItem("listaDeCompras", JSON.stringify(listaDeCompras));
+                }
+        }
+        divColumnaDer.appendChild(botonBorrar);
+
+        // creo la estructura html con jerarquías (li contiene div, que contiene 3 divs)
+        divFila.appendChild(divColumnaIzq);
+        divFila.appendChild(divColumnaMed);
+        divFila.appendChild(divColumnaDer);
+        li.appendChild(divFila);
+
+        lista.appendChild(li);
+        console.log("Agregando " + compraRecuperadaDelStorage.producto);
+        numProducto ++;
+    }
 console.log(listaDeCompras);
 }
 
@@ -58,7 +91,6 @@ class Compra{
 
 // evento cuando hacen click en Guardar item
 boton.onclick = () => {
-    console.log("Hicieron click");
     contenidoFormularioProducto = document.getElementById("inputProducto");
     contenidoFormularioCantidad = document.getElementById("inputCantidad");
 
@@ -87,11 +119,35 @@ boton.onclick = () => {
             divColumnaIzq.className = "columna izquierda";
             divColumnaMed.className = "columna centro";
             divColumnaDer.className = "columna derecha";
+            li.id="fila_" + numProducto;
 
             // guardo el contenido de cada div
             divColumnaIzq.innerHTML = contenidoFormularioProducto.value;
             divColumnaMed.innerHTML = contenidoFormularioCantidad.value;
-            divColumnaDer.innerHTML = "eliminar";
+
+            //<button id="borrar" class="botonEliminarUltimo">Eliminar último</button>
+            let botonBorrar = document.createElement("botton");
+            botonBorrar.innerHTML = "eliminar";
+            botonBorrar.className = "botonEliminar";
+            botonBorrar.id = numProducto;
+        
+            botonBorrar.onclick = () => {
+                console.log("Eliminando fila " + "fila_" + botonBorrar.id);
+                console.log(listaDeCompras)
+    
+                let eliminado = listaDeCompras[botonBorrar.id];
+                if (eliminado){   
+                    delete listaDeCompras[botonBorrar.id];     
+                    listaDeCompras.remove    
+                    console.log("por borrar el elemento " + String(botonBorrar.id));
+                    document.getElementById("fila_" + botonBorrar.id).remove();
+                    console.log ("Has eliminado "+ eliminado.producto);
+                    console.log (listaDeCompras);
+                    sessionStorage.setItem("listaDeCompras", JSON.stringify(listaDeCompras));
+                    }
+            }
+            
+            divColumnaDer.appendChild(botonBorrar);
 
             // creo la estructura html con jerarquías (li contiene div, que contiene 3 divs)
             divFila.appendChild(divColumnaIzq);
@@ -99,7 +155,6 @@ boton.onclick = () => {
             divFila.appendChild(divColumnaDer);
             li.appendChild(divFila);
             lista.appendChild(li);
-
 
             cantidadAcumulada += Number(contenidoFormularioCantidad.value);
             console.log("Acumulado: " + cantidadAcumulada);
@@ -109,6 +164,8 @@ boton.onclick = () => {
             sessionStorage.setItem("listaDeCompras", JSON.stringify(listaDeCompras));
         }
     }
+    numProducto ++;
+
 }
 
 // evento cuando hacen click en Eliminar último
